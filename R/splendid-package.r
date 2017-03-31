67b8f27e25c1a612b15072d39cf236b107c980be
Derek Chiu <dchiu@bccrc.ca>
2017-03-31 12:47:17 -0700

    short blurb on package doc

--- R/splendid-package.r
+++ R/splendid-package.r
@@ -1,5 +1,10 @@
-#' splendid.
-#'
+#' splendid
+#' 
+#' The splendid package provides an ensemble framework for classifying,
+#' predicting, and evaluating supervised learning problems. An ensemble is built
+#' across bootstrap resamples as well as across different machine learning
+#' classification algorithms.
+#' 
 #' @name splendid
 #' @docType package
 #' @importFrom magrittr "%>%"
