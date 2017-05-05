28090f507349832636493f2e168b58d07bf00bff
Derek Chiu <dchiu@bccrc.ca>
2017-05-05 13:27:23 -0700

    add temp code for #17

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,25 @@ sink_output <- function(expr) {
   sink()
   return(mod)
 }
+
+# Temp code for #17
+# Load data and run splendid on all algorithms
+data(hgsc)
+class <- stringr::str_split_fixed(rownames(hgsc), "_", n = 2)[, 2]
+sl_result <- splendid(hgsc, class, n = 1)
+mods <- purrr::map(sl_result$models, 1)
+
+# Testing on training set here, but use test set in real application
+predict(mods$lda, hgsc)$posterior
+predict(mods$rf, hgsc, "prob")
+predict(mods$multinom, hgsc, "probs")
+predict(mods$nnet, hgsc)
+# no knn since it uses non-parametric classification
+predict(mods$svm, hgsc, probability = TRUE) %>% attr("prob")
+pamr::pamr.predict(mods$pam, t(hgsc), threshold = 1, type = "posterior")
+predict(mods$adaboost, hgsc %>% set_names(make.names(names(.))), "prob")
+predict(mods$xgboost, as.matrix(hgsc)) %>% 
+  matrix(ncol = mods$xgboost$params$num_class, byrow = TRUE)
+predict(mods$nb, hgsc, "raw")
+predict(mods$lasso, as.matrix(hgsc), type = "response")
+predict(mods$ridge, as.matrix(hgsc), type = "response")
