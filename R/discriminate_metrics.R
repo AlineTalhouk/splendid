936414ff554049379e7656dd60fe6bdb95747e7e
Dustin21 <dustin.bwj@gmail.com>
2017-05-08 03:00:13 -0700

    add logloss, m-index/auc, pdi, discr plot - dchiu911 please update evaluation.R

---
+++ R/discriminate_metrics.R
@@ -0,0 +1,64 @@
+#' Multi-class Log/cross-entropy Loss 
+#' @references https://cran.r-project.org/web/packages/MLmetrics/MLmetrics.pdf 
+#' @noRd
+logloss <- function(x, pred.probs)
+{
+    if (is.matrix(y_true) == FALSE) {
+        y_true <- model.matrix(~0 + ., data.frame(as.character(y_true)))
+    }
+    eps <- 1e-15
+    N <- nrow(y_pred)
+    y_pred <- pmax(pmin(y_pred, 1 - eps), eps)
+    MultiLogLoss <- (-1/N) * sum(y_true * log(y_pred))
+    return(MultiLogLoss)
+}
+
+#' AUC/M-index: Multiple Class Area under ROC Curve
+#' @references http://link.springer.com/article/10.1023/A:1010920819831
+#' @noRd
+auc <- function(x, pred.probs)
+{
+	# ui-constructor for multicap class
+	mcap.construct <- HandTill2001::multcap(response = x, predicted = as.matrix(pred.probs))	
+	
+	# multi-class auc metric	
+	auc.out <- HandTill2001::auc(mcap.construct)
+
+	return(auc.out)
+}
+
+#' Polytomous discrimination index (PDI)
+#' @references http://onlinelibrary.wiley.com/doi/10.1002/sim.5321/abstract
+#' @noRd
+pdi <- function(C) 
+{
+	# pending	
+}
+
+#' Discrimination plot: boxplots of the predicted probabilities for each
+#' outcome category according to each observed outcome category
+#' @references http://onlinelibrary.wiley.com/doi/10.1002/sim.5321/abstract
+#' @noRd
+discrimination.plot <- function(x, pred.probs)
+{
+	require(ggplot2)
+
+	df <- data.frame(trueClass = x, pred.probs)
+
+	# turn into long-form for plotting
+	df.long <- reshape2::melt(df, variable.name = "class", value.name = "prob", id.vars = "trueClass")
+	
+	# create prevailance (base-line) class proportion table
+	df.prevalence <- df.long %>%
+		group_by(trueClass) %>% 
+		summarise(classCount = length(trueClass)) %>%
+		mutate(totalCount = sum(classCount)) %>%
+		mutate(prevalence = classCount / totalCount)
+
+	# discrimination plot
+	p <- ggplot(df.long, aes(x = class, y = prob, fill = class)) + geom_boxplot(alpha = 0.6) + 
+			facet_wrap(~trueClass) + 
+			xlab("Outcome category") + ylab("Risk of observed outcome") + theme_bw() + 
+			geom_hline(data = df.prevalence, aes(yintercept = prevalence), colour = "lightgrey")
+	return(p)
+}
