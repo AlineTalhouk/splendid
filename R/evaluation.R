e7b09fa37d225c64a6792264166e5b99503637ea
Derek Chiu <dchiu@bccrc.ca>
2017-05-12 11:19:17 -0700

    call logloss directly from package but reverse params

--- R/evaluation.R
+++ R/evaluation.R
@@ -26,7 +26,7 @@
 #' test.id <- which(!seq_along(class) %in% training.id)
 #' mod <- classification(hgsc[training.id, ], class[training.id], "xgboost")
 #' pred <- prediction(mod, hgsc, test.id, class)
-#' evaluation(class[test.id], pred, plot = TRUE)
+#' evaluation(class[test.id], pred)
 evaluation <- function(x, y, plot = FALSE) {
   # Multiclass confusion matrix with actual as rows, predicted as columns
   cm <- as.matrix(table(Actual = x, Predicted = y))
@@ -118,17 +118,9 @@ ova <- function(C) {
 #' Multi-class Log/cross-entropy Loss
 #' @param x actual class labels
 #' @param pred.probs predicted probabilities for each class
-#' @references https://cran.r-project.org/web/packages/MLmetrics/MLmetrics.pdf
 #' @noRd
 logloss <- function(x, pred.probs) {
-  if (!is.matrix(x)) {
-    x <- stats::model.matrix(~ 0 + ., data.frame(as.character(x)))
-  }
-  eps <- 1e-15
-  N <- nrow(pred.probs)
-  pred.probs <- t(apply(pred.probs, 1, function(x) pmax(pmin(x, 1 - eps), eps)))
-  MultiLogLoss <- (-1 / N) * sum(x * log(pred.probs))
-  return(MultiLogLoss)
+  MLmetrics::MultiLogLoss(pred.probs, x)
 }
 
 #' AUC/M-index: Multiple Class Area under ROC Curve
@@ -144,83 +136,4 @@ auc <- function(x, pred.probs) {
   # multi-class auc metric
   auc.out <- HandTill2001::auc(mcap.construct)
   return(auc.out)
-}
-
-#' Discrimination plot: boxplots of the predicted probabilities for each
-#' outcome category according to each observed outcome category
-#' @references http://onlinelibrary.wiley.com/doi/10.1002/sim.5321/abstract
-#' @noRd
-discrimination_plot <- function(x, pred.probs) {
-
-  # turn into long-form for plotting
-  df.long <- data.frame(trueClass = x, pred.probs) %>%
-    tidyr::gather(key = "class", value = "prob", -1, factor_key = TRUE)
-
-  # create prevalance (base-line) class proportion table
-  df.prevalence <- df.long %>%
-    dplyr::group_by_("trueClass") %>%
-    dplyr::summarise_("classCount" = ~ length(trueClass)) %>%
-    dplyr::mutate_("totalCount" = ~ sum(classCount),
-                   "prevalence" = ~ classCount / totalCount)
-
-  # discrimination plot
-  cols <- grDevices::rainbow(dplyr::n_distinct(x))
-  p <- ggplot(df.long, aes_(x = ~class, y = ~prob, fill = ~class)) +
-    geom_boxplot(alpha = 0.6) +
-    geom_hline(data = df.prevalence, aes_(yintercept = ~prevalence),
-               colour = "lightgrey") +
-    scale_fill_manual(values = cols) +
-    facet_wrap(~trueClass) +
-    labs(title = "Discrimination Plot by True Class", x = "Predicted Class",
-         y = "Risk of Predicted Class") +
-    theme_bw() +
-    theme(plot.title = element_text(face = "bold"),
-          panel.grid = element_blank(),
-          legend.position = "none")
-  print(p)
-}
-
-#' Reliability plot: mean prediction vs. observed fraction on lowess smoother
-#' for each class
-#' @noRd
-reliability_plot <- function(x, pred.probs) {
-
-  # cut each class into probability bins of 10, fit lowess
-  df <- levels(x) %>%
-    purrr::set_names() %>%
-    purrr::map(~ {
-      prob <- pred.probs[, .x]
-      cl <- ifelse(x == .x, 1, 0)
-      bin.pred <- cut(prob, 10)
-      purrr::map_df(levels(bin.pred), ~ {
-        idx <- .x == bin.pred
-        data.frame(V1 = mean(prob[idx]),
-                   V2 = sum(cl[idx]) / length(cl[idx]))
-      }) %>%
-        dplyr::filter(!is.nan(V1)) %>%
-        with(., stats::lowess(V1, V2))
-    }) %>%
-    purrr::map2(names(.), ~ c(.x, class = .y)) %>%
-    purrr::map(tibble::as.tibble) %>%
-    dplyr::bind_rows() %>%
-    dplyr::mutate(class = factor(class))
-
-  # reliability plot
-  cols <- grDevices::rainbow(dplyr::n_distinct(x))
-  p <- ggplot(df, aes_(~x, ~y, group = ~class, colour = ~class)) +
-    geom_line(lwd = 2) +
-    geom_abline(intercept = 0, slope = 1, color = "grey") +
-    scale_color_manual(values = cols) +
-    scale_x_continuous(breaks = seq(0, 1, 0.2), limits = c(0, 1)) +
-    scale_y_continuous(breaks = seq(0, 1, 0.2)) +
-    labs(x = "Mean Prediction", y = "Observed Fraction",
-         title = "Reliability Plot") +
-    theme_bw() +
-    theme(plot.title = element_text(face = "bold"),
-          panel.grid = element_blank(),
-          legend.title = element_blank(),
-          legend.position = c(0, 1),
-          legend.justification = c(0, 1),
-          legend.box.margin = margin(5, 0, 0, 5))
-  print(p)
 }
