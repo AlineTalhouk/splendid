2bf7fda94c92117654c13872cf73d1310d7f9902
Derek Chiu <dchiu@bccrc.ca>
2017-05-14 01:13:35 -0700

    class param no longer in the 4th position for args

--- R/graphs.R
+++ R/graphs.R
@@ -28,7 +28,7 @@
 #' training.id <- sample(seq_along(class), replace = TRUE)
 #' test.id <- which(!seq_along(class) %in% training.id)
 #' mod <- classification(hgsc[training.id, ], class[training.id], "xgboost")
-#' pred <- prediction(mod, hgsc, test.id, class)
+#' pred <- prediction(mod, hgsc, test.id, class = class)
 #' discrimination_plot(class[test.id], attr(pred, "prob"))
 #' reliability_plot(class[test.id], attr(pred, "prob"))
 discrimination_plot <- function(x, pred.probs) {
--- tests/testthat/test-evaluation.R
+++ tests/testthat/test-evaluation.R
@@ -7,6 +7,6 @@ test_that("evaluation plots can be outputted", {
   training.id <- sample(seq_along(class), replace = TRUE)
   test.id <- which(!seq_along(class) %in% training.id)
   mod <- classification(hgsc[training.id, ], class[training.id], "xgboost")
-  pred <- prediction(mod, hgsc, test.id, class)
+  pred <- prediction(mod, hgsc, test.id, class = class)
   expect_error(evaluation(class[test.id], pred, plot = TRUE), NA)
 })
