526199970c64e3a309ea2b4122e7cff3a7423bbb
Derek Chiu <dchiu@bccrc.ca>
2017-06-06 10:08:06 -0700

    at_depth is deprecated; use modify_depth

--- R/splendid_model.R
+++ R/splendid_model.R
@@ -29,9 +29,9 @@ splendid_model <- function(data, class, n, seed = 1, algorithms = NULL,
                   threshold = threshold, ...))
   evals <- preds %>% purrr::map(
     ~ purrr::map2(test.idx, .x, ~ evaluation(class[.x], .y))) %>%
-    purrr::at_depth(2, ~ purrr::flatten(.x) %>%
-                      unlist() %>%
-                      data.frame()) %>%
+    purrr::modify_depth(2, ~ purrr::flatten(.x) %>%
+                          unlist() %>%
+                          data.frame()) %>%
     purrr::map(~ data.frame(.x) %>% magrittr::set_colnames(seq_len(n)))
   dplyr::lst(models, preds, evals)
 }
