6b53c185bc237fb5772aca8086157783921a2a2f
Derek Chiu <dchiu@bccrc.ca>
2017-06-08 16:45:28 -0700

    rewrite ova_train and ova_predict to adhere to splendid format

--- sequential_oneVsAll/oneVsAll.R
+++ sequential_oneVsAll/oneVsAll.R
@@ -1,34 +1,34 @@
-oneVsAll_train <- function(dat, class)
-###########################################################
-# Train One-vs-All approach
-# Inputs:
-#   :dat:   (data.frame) predictors on which to train the model
-#   :class: (numeric) true training classes
-# Outputs:
-#   :fits: (list) fit binary classifier on each class
-###########################################################
-{
-  class.df <- binarize__(data.frame(class)) %>% dplyr::select(-class)
-  fits <- purrr::map(class.df, ~splendid::classification(data = dat %>% as.matrix, class = .x, algs = NULL))
-  return(fits)
+#' One-Vs-All training approach
+#'
+#' @inheritParams splendid
+#' @return list of  binary classifier fits on each class
+#' @author Dustin Johnson, Derek Chiu
+#' @export
+ova_train <- function(data, class, algs) {
+  fits <- data.frame(class) %>%
+    binarize() %>%
+    dplyr::select(-class) %>%
+    purrr::map(classification, data = data, algs = algs)
+  fits
 }
 
-oneVsAll_pred <- function(fit, dat, class.true)
-##################################################################
-# Predict using the One-vs-All approach
-# Inputs:
-#   :fit:        (list) output from the oneVsAll_train function
-#   :dat:        (data.frame) predictors on which to train the model
-#   :class.true: (numeric) vector of true training classes
-# Outputs:
-#   :pred.probs: (tibble) predicted probabilities for each class
-##################################################################
-{
-  pred.probs <- purrr::map(fit, ~splendid::prediction(.x, dat, probability = TRUE, class = class.true) %@% "prob") %>%
-    purrrlyr::dmap(~.x[,2]) %>% 
-    data.table::setattr(., "class.true", class.true) %>%
-    data.table::setattr(., "class.pred", apply(., 1, which.max))
-  return(pred.probs)
+#' One-Vs-All prediction approach
+#'
+#' @param fits list of ova fits from \code{ova_train}
+#' @inheritParams splendid
+#' @return (tibble) predicted probabilities for each class
+#' @author Dustin Johnson, Derek Chiu
+#' @export
+ova_predict <- function(fits, data, test.id, class, threshold = 0.5) {
+  prob <- fits %>%
+    purrr::map(prediction, data = data, test.id = test.id, class = class) %>%
+    purrr::map(`%@%`, "prob") %>%
+    purrr::map_df(~ .x[, 2]) %>%
+    as.matrix() %>%
+    magrittr::set_rownames(rownames(data[test.id, ]))
+  pred <- factor(unique(class)[max.col(prob)])
+  ct <- class_threshold(prob, threshold = threshold)
+  cp <- class_proportion(ct)
+  structure(pred, prob = prob, class.true = class[test.id], class.thres = ct,
+            class.prop = cp)
 }
-
-
