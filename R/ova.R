953dc6075a391e79e67d0bfba482d8384ac896a3
Derek Chiu <dchiu@bccrc.ca>
2017-06-09 10:39:11 -0700

    ensure ova prob matrix has normalized probs that sum to one

--- R/ova.R
+++ R/ova.R
@@ -4,11 +4,11 @@
 #' @return list of  binary classifier fits on each class
 #' @author Dustin Johnson, Derek Chiu
 #' @export
-ova_train <- function(data, class, algs) {
+ova_train <- function(data, class, algs, rfe) {
   fits <- data.frame(class) %>%
     binarize() %>%
     dplyr::select(-class) %>%
-    purrr::map(classification, data = data, algs = algs)
+    purrr::map(classification, data = data, algs = algs, rfe = rfe)
   fits
 }
 
@@ -19,12 +19,13 @@ ova_train <- function(data, class, algs) {
 #' @return (tibble) predicted probabilities for each class
 #' @author Dustin Johnson, Derek Chiu
 #' @export
-ova_predict <- function(fits, data, test.id, class, threshold = 0.5) {
+ova_predict <- function(fits, data, test.id, class, threshold = 0.5, ...) {
   prob <- fits %>%
     purrr::map(prediction, data = data, test.id = test.id, class = class) %>%
     purrr::map(`%@%`, "prob") %>%
     purrr::map_df(~ .x[, 2]) %>%
     as.matrix() %>%
+    sum_to_one() %>%
     magrittr::set_rownames(rownames(data[test.id, ]))
   pred <- factor(unique(class)[max.col(prob)])
   ct <- class_threshold(prob, threshold = threshold)
