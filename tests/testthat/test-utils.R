17619dd29a7d5bf95a26c8c3f6d062ca8e8b0e96
Derek Chiu <dchiu@bccrc.ca>
2017-06-12 12:25:11 -0700

    missed package namespace

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,7 +3,7 @@ context("utils")
 test_that("sum_to_one assigns random class when probs all equal zero", {
   set.seed(1)
   prob <- matrix(c(0.1, 0.2, 0.3, 0.39, 0, 0, 0, 0), nrow = 2, byrow = TRUE)
-  runs <- rerun(10, sum_to_one(prob))
+  runs <- purrr::rerun(10, sum_to_one(prob))
   expect_equal(
     purrr::map(runs, `[`, 2, 1:4) %>%
       purrr::map(`==`, 1) %>%
