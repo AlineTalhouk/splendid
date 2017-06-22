f7f76101f9aec3348531a77c196cc4a81c41d6e3
Derek Chiu <dchiu@bccrc.ca>
2017-06-21 17:06:57 -0700

    missed this deprecated name too

--- tests/testthat/test-ova.R
+++ tests/testthat/test-ova.R
@@ -14,6 +14,6 @@ test_that("mlr_nnet and nnet ova outputs need matrix coercion", {
   sl_result <- splendid_model(dat, class, n = 1,
                               algorithms = c("mlr_nnet", "nnet"),
                               ova = TRUE)
-  expect_is(sl_result$preds$ova_multinom_nnet[[1]] %@% "prob", "matrix")
+  expect_is(sl_result$preds$ova_mlr_nnet[[1]] %@% "prob", "matrix")
   expect_is(sl_result$preds$ova_nnet[[1]] %@% "prob", "matrix")
 })
