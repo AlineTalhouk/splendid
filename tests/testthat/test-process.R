2a7cf9ab331b75a1c614216ed88a094cba972d65
Derek Chiu <dchiu@bccrc.ca>
2019-05-28 15:51:07 -0700

    add unit tests

--- tests/testthat/test-process.R
+++ tests/testthat/test-process.R
@@ -19,3 +19,16 @@ test_that("error message is thrown for certain algorithms", {
   )
   expect_error(splendid_process(iris, algorithms = "lda", convert = FALSE))
 })
+
+test_that("subsampling methods create different number of cases", {
+  iris2 <- iris[1:130, ]
+  iris_orig <- subsample(iris2, iris2$Species, sampling = "none")
+  iris_up <- subsample(iris2, iris2$Species, sampling = "up")
+  iris_down <- subsample(iris2, iris2$Species, sampling = "down")
+  iris_smote <- subsample(iris2, iris2$Species, sampling = "smote")
+
+  expect_equal(nrow(iris_orig), 130)
+  expect_equal(nrow(iris_up), 150)
+  expect_equal(nrow(iris_down), 90)
+  expect_equal(nrow(iris_smote), 210)
+})
