ad8b2bd84b24b090738824075f4ce758f1c9c8fe
Derek Chiu <dchiu@bccrc.ca>
2019-08-12 16:37:13 -0700

    add tuning unit tests

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -1,8 +1,23 @@
 context("classification")
 
+data(hgsc)
+dat <- hgsc[1:15, 1:5]
+class <- attr(hgsc, "class.true")[1:15]
+
 test_that("RFE-SVM works", {
-  data(hgsc)
-  dat <- hgsc[1:15, 1:5]
-  class <- attr(hgsc, "class.true")[1:15]
   expect_error(classification(dat, class, "svm", rfe = TRUE, sizes = 5), NA)
+})
+
+test_that("RF tuning works", {
+  c1 <- classification(dat, class, "rf", tune = TRUE)
+  expect_is(c1, "train")
+})
+
+test_that("RF tuning can be reproducible", {
+  c2 <- classification(dat, class, "rf", tune = TRUE, seed_alg = 2, trees = 100)
+  set.seed(2)
+  c3 <- randomForest::randomForest(x = dat, y = factor(class), ntree = 100,
+                                   mtry = c2$finalModel$mtry)
+
+  expect_equal(c2$finalModel$votes, c3$votes)
 })
