551685ce2df6c71fa9624fb125e4e034f55511cf
Derek Chiu <dchiu@bccrc.ca>
2019-09-16 16:11:38 -0700

    test boot_train() recursive case

--- tests/testthat/test-boot.R
+++ tests/testthat/test-boot.R
@@ -12,3 +12,12 @@ test_that("stratified bootstrap samples preserve class proportions", {
   counts2 <- purrr::map(id2, ~ as.vector(table(iris2[., "Species"])))
   expect_true(all(unlist(purrr::map(counts2, ~ . == counts))))
 })
+
+test_that("bootstrap runs recursively when there's extreme imbalance", {
+  set.seed(1)
+  iris2 <- iris
+  iris2$Species[101:147] <- "versicolor"
+
+  id <- boot_train(iris2, iris2$Species, n = 10, stratify = FALSE)
+  expect_length(id, 10)
+})
