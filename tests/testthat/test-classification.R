34a701a1c9ee8713013675fdfb9482bdd519b02e
Derek Chiu <dchiu@bccrc.ca>
2019-09-17 12:46:54 -0700

    increase test coverage in classification()

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -8,7 +8,12 @@ test_that("RFE-SVM works", {
   expect_error(classification(dat, class, "svm", rfe = TRUE, sizes = 5), NA)
 })
 
-test_that("RF tuning works", {
+test_that("RF with no tuning and NULL trees uses randomForest defaults", {
+  rf_1 <- classification(dat, class, "rf", rfe = TRUE, tune = FALSE, trees = NULL, seed_alg = 1)
+  expect_equal(rf_1$ntree, 500)
+})
+
+test_that("RF tuning and prediction works", {
   c1 <- classification(dat, class, "rf", tune = TRUE)
   expect_is(c1, "train")
 
@@ -25,11 +30,50 @@ test_that("RF tuning can be reproducible", {
   expect_equal(c2$finalModel$votes, c3$votes)
 })
 
+test_that("LDA doesn't need tuning", {
+  lda_1 <- classification(dat, class, "lda", tune = FALSE)
+  lda_2 <- classification(dat, class, "lda", tune = TRUE)
+  expect_equal(lda_1[1:7], lda_2$finalModel[1:7])
+})
+
+test_that("SVM tuning works", {
+  svm_1 <- sink_output(classification(dat, class, "svm", tune = TRUE))
+  expect_equal(nrow(svm_1$results), 25)
+})
+
 test_that("pam is reproducible", {
-  pam_1 <- classification(dat, class, "pam")
+  pam_1 <- classification(dat, class, "pam", seed_alg = 1)
   pam_2a <- classification(dat, class, "pam", seed_alg = 2)
   pam_2b <- classification(dat, class, "pam", seed_alg = 2)
 
-  expect_false(identical(pam_1, pam_2a))
-  expect_identical(pam_2a, pam_2b)
+  expect_false(isTRUE(all.equal(pam_1, pam_2a)))
+  expect_equal(pam_2a, pam_2b)
+})
+
+test_that("lasso and ridge are reproducible", {
+  dat <- hgsc[1:150, 1:5]
+  class <- attr(hgsc, "class.true")[1:150]
+
+  lasso_1 <- classification(dat, class, "mlr_lasso", seed_alg = 1)
+  lasso_2a <- classification(dat, class, "mlr_lasso", seed_alg = 2)
+  lasso_2b <- classification(dat, class, "mlr_lasso", seed_alg = 2)
+
+  expect_false(isTRUE(all.equal(lasso_1, lasso_2a)))
+  expect_equal(lasso_2a, lasso_2b)
+
+  ridge_1 <- classification(dat, class, "mlr_ridge", seed_alg = 1)
+  ridge_2a <- classification(dat, class, "mlr_ridge", seed_alg = 2)
+  ridge_2b <- classification(dat, class, "mlr_ridge", seed_alg = 2)
+
+  expect_false(isTRUE(all.equal(ridge_1, ridge_2a)))
+  expect_equal(ridge_2a, ridge_2b)
+})
+
+test_that("boosting is reproducible", {
+  boost_1 <- classification(dat, class, "adaboost", seed_alg = 1)
+  boost_2a <- classification(dat, class, "adaboost", seed_alg = 2)
+  boost_2b <- classification(dat, class, "adaboost", seed_alg = 2)
+
+  expect_false(isTRUE(all.equal(boost_1, boost_2a)))
+  expect_equal(boost_2a, boost_2b)
 })
